92aeea6ca79748715c3f4029f76471d7b74fae93
Cedric <cedricholz@berkeley.edu>
2018-05-03 16:11:58 -0700

    tweepy potential fix

--- limit_strategy.py
+++ limit_strategy.py
@@ -251,6 +251,12 @@ class MyStreamListener(tweepy.StreamListener):
 
 # Begin Listening for new Tweets
 utils.print_and_write_to_logfile("AWAITING TWEETS...")
-streamListener = MyStreamListener()
-stream = tweepy.Stream(auth, streamListener)
-stream.filter(follow=twitter_user_ids_to_follow)
+
+while True:
+    try:
+        streamListener = MyStreamListener()
+        stream = tweepy.Stream(auth, streamListener, timeout=60)
+        stream.filter(follow=twitter_user_ids_to_follow)
+    except Exception as e:
+        print("Restarting Stream... ")
+        print(e.message)
